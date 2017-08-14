e0d8425c9e7303de1891f46510023b468f674977
Christie Lynam <christielynam@gmail.com>
2017-08-14 17:39:06 -0600

    initil commit iteration 0

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,15 @@
 export default class DistrictRepository {
+  constructor(kinderData) {
+    this.data = this.filterData(kinderData)
+  }
 
+  filterData(kinderData) {
+    return kinderData.reduce((acc, obj) => {
+      if (!acc[obj.Location]) {
+        acc[obj.Location] = [];
+      }
+      acc[obj.Location].push(obj);
+      return acc;
+    }, {})
+  }
 }
