d05248fb3f98ef6c6fa1d3fa7e1060af9ab3e7e5
Margo Schaedel <margoschaedel@gmail.com>
2017-08-16 16:50:47 -0600

    Add header image and some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,16 @@ import Search from './Search';
 import PropTypes from 'prop-types';
 
 const Header = (props) => {
+
+    const imageStyles = {
+      backgroundImage: 'url("https://cdn.pixabay.com/photo/2012/04/14/16/54/color-pencils-34595_960_720.png")',
+      backgroundPosition: 'center',
+      backgroundSize: 'contain',
+    }
     return (
-      <div className='header'>
+      <div className='header'
+           style={imageStyles}
+           >
         <h1 className='header-title'>Headcount 2.0... School is Kewl</h1>
         <Search search={props.search} />
       </div>
