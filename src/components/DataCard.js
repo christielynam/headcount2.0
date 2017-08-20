fa1df72520e5854cd4358e04fdf7e55e4518483c
Margo Schaedel <margoschaedel@gmail.com>
2017-08-20 12:46:11 -0600

    Add additional prop types

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -29,6 +29,7 @@ const DataCard = ({ location, data, clicked, handleClick }) => {
 DataCard.propTypes = {
   location: PropTypes.string,
   data: PropTypes.object,
+  clicked: PropTypes.bool,
   handleClick: PropTypes.func,
 }
 
