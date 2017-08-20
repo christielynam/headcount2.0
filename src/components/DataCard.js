86ef54267d2fcb3a3329f552ed86aa04cf0420a4
Christie Lynam <christielynam@gmail.com>
2017-08-20 15:29:44 -0600

    add tests for App component

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -3,25 +3,18 @@ import PropTypes from 'prop-types';
 
 const DataCard = ({ location, data, clicked, handleClick }) => {
 
-  const colorIndicator = {
-    color: '#E71D36',
-  }
-
   const yearKeys = Object.keys(data)
-  const cardContent = yearKeys.map( (year, i) => {
-    if (data[year] >= 0.5) {
-      return <p className='school-data' style={colorIndicator} key={i}>{year} : {data[year]}</p>
-    } else {
-       return <p className='school-data' key={i}>{year} : {data[year]}</p>
-    }
-  })
+  const cardContent = yearKeys.map( (year, i) =>
+    data[year] >= 0.5 ? <li className='school-data' style={{color: '#E71D36'}} key={i}>{year} : {data[year]}</li> : <li className='school-data' key={i}>{year} : {data[year]}</li> )
 
   const toggleSelected = clicked ? 'selected-data-card' : 'data-card'
 
   return (
-      <div className={toggleSelected} onClick={ () => handleClick(location) }>
+      <div className={toggleSelected}
+           onClick={ () => handleClick(location) }
+      >
         <h3 className='school-district'>{location}</h3>
-        {cardContent}
+        <ul className='data-list'>{cardContent}</ul>
       </div>
   );
 }
