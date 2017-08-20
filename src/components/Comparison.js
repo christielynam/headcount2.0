44c85f6ca68a092c0334a136e9318b65b588576e
Christie Lynam <christielynam@gmail.com>
2017-08-20 13:07:19 -0600

    fix comparison card

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -3,30 +3,26 @@ import PropTypes from 'prop-types';
 
 const Comparison = ({ comparedData, compareAverages }) => {
 
-  // averageOne = district.findAverage(comparedData[0].location)
-
+  const comparedObj = compareAverages(comparedData[0].location, comparedData[1].location)
+  const ratio = comparedObj.compared
   const comparedAverages = comparedData.map(obj => {
     return (<article key={obj.location}>
       <h3>{obj.location}</h3>
-      <p>{obj.average}</p>
+      <p>{comparedObj[obj.location]}</p>
     </article>)
   })
 
-  // const ratio = (compareAverages(comparedData[0].location, comparedData[1].location)).compared
-
-  const ratio = Math.round((comparedData[0].average / comparedData[1].average) * 1000) / 1000
-
-    return (
-      <div className='comparison'>
-        <section className='top-section'>
-          {comparedAverages}
-        </section>
-        <section className='ratio'>
-          <h3>Ratio: </h3>
-          <p>{ratio}</p>
-        </section>
-      </div>
-    );
+  return (
+    <div className='comparison'>
+      <section className='top-section'>
+        {comparedAverages}
+      </section>
+      <section className='ratio'>
+        <h3>Ratio: </h3>
+        <p>{ratio}</p>
+      </section>
+    </div>
+  )
 }
 
 Comparison.propTypes = {
