6a0e30162f4348372827f590b37a32490093411e
Margo Schaedel <margoschaedel@gmail.com>
2017-08-20 22:35:05 -0600

    Add responsive layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,8 @@ import PropTypes from 'prop-types';
 
 const Header = ({ search, input }) => {
 
-    const imageStyles = {
-      // backgroundImage: 'url("/assets/colored-pencils-png.png")',
-      backgroundImage: 'url("https://cdn.pixabay.com/photo/2012/04/14/16/54/color-pencils-34595_960_720.png")',
-      backgroundPosition: 'center',
-      backgroundSize: 'contain',
-    }
     return (
-      <div className='header'
-           style={imageStyles}
-           >
+      <div className='header'>
         <h1 className='header-title'>Headcount 2.0... School is Kewl</h1>
         <Search
           search={search}
